58b43932b2be068db7480240a5a5824475e89d32
guilhermt <guilhermemartinsteixeiras@gmail.com>
2023-01-04 18:41:24 -0300

    port setup

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const weather = require('./utils/weather');
 const geocode = require('./utils/geocode');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 console.log(process.env.PORT);
 
